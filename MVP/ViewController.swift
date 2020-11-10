512f408dac802c97159e2bb662194b43d7e2d897
Ilya Slipak <ilyaslipak@gmail.com>
2020-11-10 21:31:22 +0200

    initial commit

---
+++ MVP/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Ilya Slipak on 03.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
